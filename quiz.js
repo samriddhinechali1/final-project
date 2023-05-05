8a7c180d5dedb04f382b2899fc7804ff99cc4eb2
samriddhinechali1 <120707106+samriddhinechali1@users.noreply.github.com>
2023-05-05 11:00:41 +0530

    quiz js

---
+++ quiz.js
@@ -0,0 +1,8 @@
+function submitButton(){
+     alert("Thank you for visiting!")
+  }
+//   document.getElementById("myButton").addEventListener("click", submitButton())  
+// document.getElementById("mail").addEventListener("focusin", focusText());
+// function focusText(){
+//     document.getElementById("mail").style.backgroundColor = "lightblue";
+// }
