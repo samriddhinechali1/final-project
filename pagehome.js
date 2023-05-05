92afae454c81d07cc55eca82da2403f874dff77d
samriddhinechali1 <120707106+samriddhinechali1@users.noreply.github.com>
2023-05-05 10:44:06 +0530

    homepage js

---
+++ pagehome.js
@@ -0,0 +1,62 @@
+let aus = document.getElementById('try');
+
+aus.onmouseover = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+aus.onmouseout = function() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+let ind = document.getElementById('tryind');
+
+ind.onmouseover = function() {
+    document.getElementById('indpop').style.display = 'block';
+}
+ ind.onmouseout = function() {
+    document.getElementById('indpop').style.display = 'none';
+ }
+
+let mex = document.getElementById('trymex');
+
+mex.onmouseover = function(){
+  document.getElementById('mexpop').style.display = 'block';
+}
+
+mex.onmouseout = function(){
+  document.getElementById('mexpop').style.display = 'none';
+}
+
+let saf = document.getElementById('trysaf');
+saf.onmouseover = function(){
+  document.getElementById('safpop').style.display = 'block';
+}
+
+saf.onmouseout = function(){
+  document.getElementById('safpop').style.display = 'none';
+}
+let united = document.getElementById('tryuk');
+united.onmouseover = function(){
+  document.getElementById('ukpop').style.display = 'block';
+}
+
+united.onmouseout = function(){
+  document.getElementById('ukpop').style.display = 'none';
+}
+
+let vene = document.getElementById('tryven');
+vene.onmouseover = function(){
+  document.getElementById('venpop').style.display = 'block';
+}
+
+vene.onmouseout = function(){
+  document.getElementById('venpop').style.display = 'none';
+}
+
+
+
+
+
+
+
+
